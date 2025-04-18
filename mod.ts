2d42b4179c0d4cd1b24858a705addfe8c822cd12
uri <uri.valevski@gmail.com>
2025-04-18 17:15:37 +0300

    export

--- mod.ts
+++ mod.ts
@@ -1,24 +1,28 @@
 export { injectRmmbrToken } from "./src/cacher.ts";
 export {
     deepSeekGenJsonFromConvo,
-    injectDeepSeekToken
+    injectDeepSeekToken,
 } from "./src/deepseek.ts";
 export {
     geminiGenJson,
     geminiGenJsonFromConvo,
-    injectGeminiToken
+    injectGeminiToken,
 } from "./src/gemini.ts";
 export {
+    type Action,
+    type BotSpec,
+    getHistory,
+    type HistoryEvent,
     injectAccessHistory,
     injectReply,
-    makeBot, type BotSpec,
-    type HistoryEvent
+    makeBot,
+    type MessageDraft,
+    systemUser,
 } from "./src/mcpFramework.ts";
 export {
     injectOpenAiToken,
     openAiGenJson,
     openAiGenJsonFromConvo,
-    openAiMatching
+    openAiMatching,
 } from "./src/openai.ts";
 export { catchAiRefusesToAdhereToTyping, type ModelOpts } from "./src/utils.ts";
-
