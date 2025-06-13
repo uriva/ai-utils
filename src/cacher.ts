3b16f73142da45a9ec639f963952e0849806b592
uri <uri.valevski@gmail.com>
2025-06-13 15:59:19 +0300

    fix

--- src/cacher.ts
+++ src/cacher.ts
@@ -1,7 +1,12 @@
 import { context } from "context-inject";
 import type { Func } from "gamla";
 
-const injection = context(
+const injection: {
+  inject: (
+    fn: (_cacheId: string) => <F extends Func>(_f: F) => Func,
+  ) => <F_1 extends Func>(f: F_1) => F_1;
+  access: (_cacheId: string) => <F extends Func>(_f: F) => Func;
+} = context(
   (_cacheId: string) => <F extends Func>(_f: F): Func => {
     throw new Error("cacher not injected");
   },
