3067ab405fded4007d8e542e4342082ae1f27a44
uri <uri.valevski@gmail.com>
2025-08-31 13:57:46 +0300

    reconstruct-better

--- src/gemini.ts
+++ src/gemini.ts
@@ -1,16 +1,64 @@
 import {
   type Content,
+  type FunctionDeclaration,
   type GenerateContentParameters,
   GoogleGenAI,
 } from "@google/genai";
 import { context } from "context-inject";
 import { coerce, empty, map, pipe, remove } from "gamla";
 import type { ChatCompletionMessageParam } from "openai/resources/index.mjs";
-import type { z, ZodType } from "zod/v4";
+import { z, type ZodType } from "zod/v4";
 import { makeCache } from "./cacher.ts";
-import { zodToGeminiParameters } from "./geminiAgent.ts";
 import { structuredMsgs } from "./openai.ts";
 import type { FnToSameFn, ModelOpts, TokenInjection } from "./utils.ts";
+
+// deno-lint-ignore no-explicit-any
+const isRedundantAnyMember = (x: any) =>
+  Object.keys(x).length === 1 && typeof (x.not) === "object" &&
+  Object.keys(x.not).length === 0;
+
+// deno-lint-ignore no-explicit-any
+const removeAdditionalProperties = <T>(obj: Record<string, any>) => {
+  if (typeof obj === "object" && obj !== null) {
+    let newObj = { ...obj };
+    if (obj.anyOf) {
+      // deno-lint-ignore no-explicit-any
+      newObj.anyOf = obj.anyOf.filter((x: any) =>
+        x.type !== "null" && !isRedundantAnyMember(x)
+      ).map(removeAdditionalProperties);
+    }
+    if (newObj.anyOf?.length === 1) newObj = newObj.anyOf[0];
+    if (Array.isArray(obj.type)) {
+      if (obj.type.find((x: string) => x === "null")) {
+        newObj.nullable = true;
+      }
+      newObj.type = obj.type.find((x) => x !== "null");
+    }
+    if ("additionalProperties" in newObj) {
+      newObj.additionalProperties = undefined;
+    }
+    for (const key in newObj) {
+      if (key in newObj) {
+        if (Array.isArray(newObj[key])) {
+          newObj[key] = newObj[key].map(removeAdditionalProperties);
+        } else if (
+          typeof newObj[key] === "object" && newObj[key] !== null
+        ) {
+          newObj[key] = removeAdditionalProperties(newObj[key]);
+        }
+      }
+    }
+    return newObj;
+  }
+  return obj;
+};
+
+export const zodToGeminiParameters = (zodObj: ZodType): FunctionDeclaration => {
+  const jsonSchema = removeAdditionalProperties(z.toJSONSchema(zodObj));
+  // deno-lint-ignore no-unused-vars
+  const { $schema, ...rest } = jsonSchema;
+  return rest;
+};
 
 const tokenInjection: TokenInjection = context((): string => {
   throw new Error("no gemini token injected");
